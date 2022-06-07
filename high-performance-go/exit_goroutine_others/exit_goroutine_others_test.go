3fee0cf41419515f6dd1759487aa1a15e0280998
jumper86 <jumper86@126.com>
2022-06-07 11:57:30 +0800

    更新

---
+++ high-performance-go/exit_goroutine_others/exit_goroutine_others_test.go
@@ -0,0 +1,39 @@
+package exit_goroutine_others
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func doCheckClose(taskCh chan int) {
+	for {
+		select {
+		case _, beforeClosed := <-taskCh:
+			if !beforeClosed {
+				fmt.Println("taskCh has been closed")
+				return
+			}
+			time.Sleep(time.Millisecond)
+			//fmt.Printf("task %d is done\n", t)
+		}
+	}
+}
+
+func sendTasksCheckClose() {
+	taskCh := make(chan int, 10)
+	go doCheckClose(taskCh)
+	for i := 0; i < 1000; i++ {
+		taskCh <- i
+	}
+	close(taskCh)
+}
+
+func TestDoCheckClose(t *testing.T) {
+	t.Log(runtime.NumGoroutine())
+	sendTasksCheckClose()
+	time.Sleep(time.Second)
+	runtime.GC()
+	t.Log(runtime.NumGoroutine())
+}
